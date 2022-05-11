41c36b2a6ee27b54208f469db91ce2e604aac5ee
Actricxz <bikashwork99@gmail.com>
2022-05-11 15:13:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,735 @@
+var tableData = [
+    {
+        id: "60",
+        name: "Camilla Cole",
+        email: "Eda48@yahoo.com",
+      },
+      {
+        id: "61",
+        name: "Abagail MacGyver",
+        email: "Jevon.Koch@hotmail.com",
+      },
+      {
+        id: "62",
+        name: "Ludwig Prohaska",
+        email: "Eldon_DuBuque37@yahoo.com",
+      },
+      {
+        id: "63",
+        name: "Berenice Hane",
+        email: "Lonnie3@gmail.com",
+      },
+      {
+        id: "64",
+        name: "Abel Durgan",
+        email: "Marlee.Weimann42@gmail.com",
+      },
+      {
+        id: "65",
+        name: "Tate Kuhlman",
+        email: "Hope31@yahoo.com",
+      },
+      {
+        id: "66",
+        name: "Mrs. Hermina Daniel",
+        email: "Murphy59@yahoo.com",
+      },
+      {
+        id: "67",
+        name: "Marcelino Nienow",
+        email: "Estefania_Dibbert97@hotmail.com",
+      },
+      {
+        id: "68",
+        name: "Raphaelle Connelly",
+        email: "Josie.Legros70@yahoo.com",
+      },
+      {
+        id: "69",
+        name: "Mina Heaney",
+        email: "Pete.Hermann79@hotmail.com",
+      },
+      {
+        id: "70",
+        name: "Jaida Torp",
+        email: "Jonatan82@yahoo.com",
+      },
+      {
+        id: "71",
+        name: "Megane Fadel",
+        email: "Dandre82@yahoo.com",
+      },
+      {
+        id: "72",
+        name: "Angelo Schimmel V",
+        email: "Frederic54@hotmail.com",
+      },
+      {
+        id: "73",
+        name: "Jaren Ferry DDS",
+        email: "Kristian.Kilback@yahoo.com",
+      },
+      {
+        id: "74",
+        name: "Annette O'Connell",
+        email: "Domenica.Christiansen@gmail.com",
+      },
+      {
+        id: "75",
+        name: "Elenor Rohan PhD",
+        email: "Alvera.Huel48@yahoo.com",
+      },
+      {
+        id: "76",
+        name: "Linnie Beier",
+        email: "Reece_Berge@gmail.com",
+      },
+      {
+        id: "77",
+        name: "Maybelle Lehner",
+        email: "Arlo.Hagenes@gmail.com",
+      },
+      {
+        id: "78",
+        name: "Hailie Haag",
+        email: "Edythe.Lind59@hotmail.com",
+      },
+      {
+        id: "79",
+        name: "Marcelina Schuppe",
+        email: "Kip.Cummerata77@gmail.com",
+      },
+      {
+        id: "80",
+        name: "Ezra Lang",
+        email: "Garret.Swaniawski@hotmail.com",
+      },
+      {
+        id: "81",
+        name: "Carey Kiehn",
+        email: "Sabrina70@gmail.com",
+      },
+    {
+      id: "22",
+      name: "Quentin O'Kon",
+      email: "Amely_Cummings2@yahoo.com",
+    },
+    {
+      id: "23",
+      name: "Ms. Gabriella Kunde",
+      email: "Lorenza_Cummerata@hotmail.com",
+    },
+    {
+      id: "24",
+      name: "Gerald Reilly",
+      email: "Lia_Konopelski@gmail.com",
+    },
+    {
+      id: "25",
+      name: "Brody Carter I",
+      email: "Colten.Wilderman90@hotmail.com",
+    },
+    {
+      id: "26",
+      name: "Alanis Klocko",
+      email: "Johathan.Champlin69@gmail.com",
+    },
+    {
+      id: "27",
+      name: "Caroline Miller",
+      email: "Delaney.Kozey74@gmail.com",
+    },
+    {
+      id: "28",
+      name: "Ms. Merritt McClure",
+      email: "Wendy.Zieme@gmail.com",
+    },
+    {
+      id: "29",
+      name: "Jovani Schoen",
+      email: "Norval_Zieme@hotmail.com",
+    },
+    {
+      id: "30",
+      name: "Berniece Bradtke",
+      email: "Okey.Fisher39@hotmail.com",
+    },
+    {
+      id: "31",
+      name: "Hanna Gottlieb",
+      email: "Edna62@hotmail.com",
+    },
+    {
+      id: "32",
+      name: "Christiana Bergnaum",
+      email: "Donnie.Paucek22@gmail.com",
+    },
+    {
+      id: "33",
+      name: "Reid Stehr",
+      email: "Hiram69@gmail.com",
+    },
+    {
+      id: "34",
+      name: "Evans Ward",
+      email: "Liliane72@hotmail.com",
+    },
+    {
+      id: "35",
+      name: "Miss Enos Davis",
+      email: "Kolby.Grady@hotmail.com",
+    },
+    {
+      id: "36",
+      name: "Mathew Lueilwitz",
+      email: "Mariah.Hagenes@yahoo.com",
+    },
+    {
+      id: "37",
+      name: "Romaine Robel",
+      email: "Sheila19@yahoo.com",
+    },
+    {
+      id: "38",
+      name: "Immanuel Aufderhar",
+      email: "Aileen32@hotmail.com",
+    },
+    {
+      id: "39",
+      name: "Julius Reilly Jr.",
+      email: "Santa89@yahoo.com",
+    },
+    {
+      id: "40",
+      name: "Linnea Borer",
+      email: "Jarrett_Hegmann2@hotmail.com",
+    },
+    {
+      id: "41",
+      name: "Misael Abernathy IV",
+      email: "Robbie42@yahoo.com",
+    },
+    {
+      id: "42",
+      name: "Mr. Gerda Jerde",
+      email: "Gustave36@hotmail.com",
+    },
+    {
+      id: "43",
+      name: "Easton Daugherty",
+      email: "Malcolm.Schuster11@hotmail.com",
+    },
+    {
+      id: "44",
+      name: "Leanna Gutkowski",
+      email: "Chelsea9@gmail.com",
+    },
+    {
+      id: "45",
+      name: "Rollin Trantow",
+      email: "Marques_Hickle26@gmail.com",
+    },
+    {
+      id: "46",
+      name: "Heaven Luettgen",
+      email: "Lonnie.Greenfelder95@hotmail.com",
+    },
+    {
+      id: "47",
+      name: "Gilda Huel",
+      email: "Stephania.Hintz@gmail.com",
+    },
+    {
+      id: "48",
+      name: "Javon Walker",
+      email: "Lamar_Bailey39@yahoo.com",
+    },
+    {
+      id: "49",
+      name: "Stewart King",
+      email: "Chasity_Jenkins82@gmail.com",
+    },
+    {
+      id: "50",
+      name: "Cortez Lueilwitz",
+      email: "Lelia.Mitchell43@gmail.com",
+    },
+    {
+      id: "51",
+      name: "Ora Kilback",
+      email: "Matt25@gmail.com",
+    },
+    {
+      id: "52",
+      name: "Lera Macejkovic",
+      email: "Jamal.Donnelly45@hotmail.com",
+    },
+    {
+      id: "53",
+      name: "Asa Gottlieb",
+      email: "Carolyne.Langosh@yahoo.com",
+    },
+    {
+      id: "54",
+      name: "Avery Nienow",
+      email: "Hudson_Bernhard47@yahoo.com",
+    },
+    {
+      id: "55",
+      name: "Dr. Katrine Ryan",
+      email: "Colten40@gmail.com",
+    },
+    {
+      id: "56",
+      name: "Florida Kulas",
+      email: "Alejandra_Bernhard@yahoo.com",
+    },
+    {
+      id: "57",
+      name: "Mauricio Gerlach",
+      email: "Vincenza_Gusikowski84@hotmail.com",
+    },
+    {
+      id: "58",
+      name: "Chadrick Simonis",
+      email: "Reymundo.Doyle@gmail.com",
+    },
+    {
+      id: "59",
+      name: "Beau Jenkins",
+      email: "Cletus65@hotmail.com",
+    },
+    {
+      id: "60",
+      name: "Camilla Cole",
+      email: "Eda48@yahoo.com",
+    },
+    {
+      id: "61",
+      name: "Abagail MacGyver",
+      email: "Jevon.Koch@hotmail.com",
+    },
+    {
+      id: "62",
+      name: "Ludwig Prohaska",
+      email: "Eldon_DuBuque37@yahoo.com",
+    },
+    {
+      id: "63",
+      name: "Berenice Hane",
+      email: "Lonnie3@gmail.com",
+    },
+    {
+      id: "64",
+      name: "Abel Durgan",
+      email: "Marlee.Weimann42@gmail.com",
+    },
+    {
+      id: "65",
+      name: "Tate Kuhlman",
+      email: "Hope31@yahoo.com",
+    },
+    {
+      id: "66",
+      name: "Mrs. Hermina Daniel",
+      email: "Murphy59@yahoo.com",
+    },
+    {
+      id: "67",
+      name: "Marcelino Nienow",
+      email: "Estefania_Dibbert97@hotmail.com",
+    },
+    {
+      id: "68",
+      name: "Raphaelle Connelly",
+      email: "Josie.Legros70@yahoo.com",
+    },
+    {
+      id: "69",
+      name: "Mina Heaney",
+      email: "Pete.Hermann79@hotmail.com",
+    },
+    {
+      id: "70",
+      name: "Jaida Torp",
+      email: "Jonatan82@yahoo.com",
+    },
+    {
+      id: "71",
+      name: "Megane Fadel",
+      email: "Dandre82@yahoo.com",
+    },
+    {
+      id: "72",
+      name: "Angelo Schimmel V",
+      email: "Frederic54@hotmail.com",
+    },
+    {
+      id: "73",
+      name: "Jaren Ferry DDS",
+      email: "Kristian.Kilback@yahoo.com",
+    },
+    {
+      id: "74",
+      name: "Annette O'Connell",
+      email: "Domenica.Christiansen@gmail.com",
+    },
+    {
+      id: "75",
+      name: "Elenor Rohan PhD",
+      email: "Alvera.Huel48@yahoo.com",
+    },
+    {
+      id: "76",
+      name: "Linnie Beier",
+      email: "Reece_Berge@gmail.com",
+    },
+    {
+      id: "77",
+      name: "Maybelle Lehner",
+      email: "Arlo.Hagenes@gmail.com",
+    },
+    {
+      id: "78",
+      name: "Hailie Haag",
+      email: "Edythe.Lind59@hotmail.com",
+    },
+    {
+      id: "79",
+      name: "Marcelina Schuppe",
+      email: "Kip.Cummerata77@gmail.com",
+    },
+    {
+      id: "80",
+      name: "Ezra Lang",
+      email: "Garret.Swaniawski@hotmail.com",
+    },
+    {
+      id: "81",
+      name: "Carey Kiehn",
+      email: "Sabrina70@gmail.com",
+    },
+    {
+      id: "82",
+      name: "Carole Aufderhar",
+      email: "Efrain.Schamberger77@hotmail.com",
+    },
+    {
+      id: "83",
+      name: "Fanny Beatty",
+      email: "Ewald28@yahoo.com",
+    },
+    {
+      id: "84",
+      name: "Mina Lubowitz",
+      email: "Vincenzo.Herman@gmail.com",
+    },
+    {
+      id: "85",
+      name: "Anabelle Thiel",
+      email: "Amely.Renner@hotmail.com",
+    },
+    {
+        id: "48",
+        name: "Javon Walker",
+        email: "Lamar_Bailey39@yahoo.com",
+      },
+      {
+        id: "49",
+        name: "Stewart King",
+        email: "Chasity_Jenkins82@gmail.com",
+      },
+      {
+        id: "50",
+        name: "Cortez Lueilwitz",
+        email: "Lelia.Mitchell43@gmail.com",
+      },
+      {
+        id: "51",
+        name: "Ora Kilback",
+        email: "Matt25@gmail.com",
+      },
+      {
+        id: "52",
+        name: "Lera Macejkovic",
+        email: "Jamal.Donnelly45@hotmail.com",
+      },
+      {
+        id: "53",
+        name: "Asa Gottlieb",
+        email: "Carolyne.Langosh@yahoo.com",
+      },
+      {
+        id: "54",
+        name: "Avery Nienow",
+        email: "Hudson_Bernhard47@yahoo.com",
+      },
+      {
+        id: "55",
+        name: "Dr. Katrine Ryan",
+        email: "Colten40@gmail.com",
+      },
+      {
+        id: "56",
+        name: "Florida Kulas",
+        email: "Alejandra_Bernhard@yahoo.com",
+      },
+      {
+        id: "57",
+        name: "Mauricio Gerlach",
+        email: "Vincenza_Gusikowski84@hotmail.com",
+      },
+      {
+        id: "58",
+        name: "Chadrick Simonis",
+        email: "Reymundo.Doyle@gmail.com",
+      },
+      {
+        id: "59",
+        name: "Beau Jenkins",
+        email: "Cletus65@hotmail.com",
+      },
+      {
+        id: "60",
+        name: "Camilla Cole",
+        email: "Eda48@yahoo.com",
+      },
+      {
+        id: "61",
+        name: "Abagail MacGyver",
+        email: "Jevon.Koch@hotmail.com",
+      },
+      {
+        id: "62",
+        name: "Ludwig Prohaska",
+        email: "Eldon_DuBuque37@yahoo.com",
+      },
+      {
+        id: "63",
+        name: "Berenice Hane",
+        email: "Lonnie3@gmail.com",
+      },
+      {
+        id: "64",
+        name: "Abel Durgan",
+        email: "Marlee.Weimann42@gmail.com",
+      },
+      {
+        id: "65",
+        name: "Tate Kuhlman",
+        email: "Hope31@yahoo.com",
+      },
+      {
+        id: "66",
+        name: "Mrs. Hermina Daniel",
+        email: "Murphy59@yahoo.com",
+      },
+      {
+        id: "67",
+        name: "Marcelino Nienow",
+        email: "Estefania_Dibbert97@hotmail.com",
+      },
+      {
+        id: "68",
+        name: "Raphaelle Connelly",
+        email: "Josie.Legros70@yahoo.com",
+      },
+      {
+        id: "69",
+        name: "Mina Heaney",
+        email: "Pete.Hermann79@hotmail.com",
+      },
+      {
+        id: "70",
+        name: "Jaida Torp",
+        email: "Jonatan82@yahoo.com",
+      },
+      {
+        id: "71",
+        name: "Megane Fadel",
+        email: "Dandre82@yahoo.com",
+      },
+      {
+        id: "72",
+        name: "Angelo Schimmel V",
+        email: "Frederic54@hotmail.com",
+      },
+      {
+        id: "73",
+        name: "Jaren Ferry DDS",
+        email: "Kristian.Kilback@yahoo.com",
+      },
+      {
+        id: "74",
+        name: "Annette O'Connell",
+        email: "Domenica.Christiansen@gmail.com",
+      },
+      {
+        id: "75",
+        name: "Elenor Rohan PhD",
+        email: "Alvera.Huel48@yahoo.com",
+      },
+      {
+        id: "76",
+        name: "Linnie Beier",
+        email: "Reece_Berge@gmail.com",
+      },
+      {
+        id: "77",
+        name: "Maybelle Lehner",
+        email: "Arlo.Hagenes@gmail.com",
+      },
+      {
+        id: "78",
+        name: "Hailie Haag",
+        email: "Edythe.Lind59@hotmail.com",
+      },
+      {
+        id: "79",
+        name: "Marcelina Schuppe",
+        email: "Kip.Cummerata77@gmail.com",
+      },
+      {
+        id: "80",
+        name: "Ezra Lang",
+        email: "Garret.Swaniawski@hotmail.com",
+      },
+      {
+        id: "81",
+        name: "Carey Kiehn",
+        email: "Sabrina70@gmail.com",
+      },
+      {
+        id: "82",
+        name: "Carole Aufderhar",
+        email: "Efrain.Schamberger77@hotmail.com",
+      },
+      {
+        id: "83",
+        name: "Fanny Beatty",
+        email: "Ewald28@yahoo.com",
+      },
+      {
+        id: "84",
+        name: "Mina Lubowitz",
+        email: "Vincenzo.Herman@gmail.com",
+      },
+      {
+        id: "85",
+        name: "Anabelle Thiel",
+        email: "Amely.Renner@hotmail.com",
+      },
+      {
+        id: "86",
+        name: "Gerry Homenick",
+        email: "Ada_Von54@hotmail.com",
+      },
+      {
+        id: "87",
+        name: "Domenico Maggio",
+        email: "Freeman.Stamm@yahoo.com",
+      },
+      {
+        id: "88",
+        name: "Mauricio Bradtke III",
+        email: "Alford83@yahoo.com",
+      },
+      {
+        id: "89",
+        name: "Bulah Yost III",
+        email: "Nicolas_Schimmel6@yahoo.com",
+      },
+  ];
+  
+  var state = {
+      'querySet': tableData,
+  
+      'page': 1,
+      // 'rows': 5,
+      'rows': 10,
+      'window': 5,
+  }
+  
+  buildTable()
+  
+  function pagination(querySet, page, rows) {
+  
+      var trimStart = (page - 1) * rows
+      var trimEnd = trimStart + rows
+  
+      var trimmedData = querySet.slice(trimStart, trimEnd)
+  
+      var pages = Math.round(querySet.length / rows);
+  
+      return {
+          'querySet': trimmedData,
+          'pages': pages,
+      }
+  }
+  
+  function pageButtons(pages) {
+      var wrapper = document.getElementById('pagination-wrapper')
+  
+      wrapper.innerHTML = ``
+      console.log('Pages:', pages)
+  
+      var maxLeft = (state.page - Math.floor(state.window / 2))
+      var maxRight = (state.page + Math.floor(state.window / 2))
+  
+      if (maxLeft < 1) {
+          maxLeft = 1
+          maxRight = state.window
+      }
+  
+      if (maxRight > pages) {
+          maxLeft = pages - (state.window - 1)
+          
+          if (maxLeft < 1){
+              maxLeft = 1
+          }
+          maxRight = pages
+      }
+      
+      
+  
+      for (var page = maxLeft; page <= maxRight; page++) {
+          wrapper.innerHTML += `<button value=${page} class="page btn">${page}</button>`
+      }
+  
+      if (state.page != 1) {
+          wrapper.innerHTML = `<button value=${1} class="page btn">&#171; First</button>` + wrapper.innerHTML
+      }
+  
+      if (state.page != pages) {
+          wrapper.innerHTML += `<button value=${pages} class="page btn ">Last &#187;</button>`
+      }
+  
+      $('.page').on('click', function() {
+          $('#table-body').empty()
+  
+          state.page = Number($(this).val())
+  
+          buildTable()
+      })
+  
+  }
+  
+  
+  function buildTable() {
+    var table = $('#table-body')
+
+    var data = pagination(state.querySet, state.page, state.rows)
+    var myList = data.querySet
+
+    for (let i  in myList) {
+        //Keep in mind we are using "Template Litterals to create rows"
+        var row = `<tr>
+                  <td>${myList[i].id}</td>
+                  <td>${myList[i].name}</td>
+                  <td>${myList[i].email}</td>
+                  `
+        table.append(row)
+    }
+
+    pageButtons(data.pages)
+}
